5eb28d7c2a500cf653baacf8d9ba4dd5bba65654
dichgrem <brcefy@gmail.com>
2026-03-12 15:35:49 +0800

    feat:RESTful_API

---
+++ src/main/java/com/stu/helloserver/common/Result.java
@@ -0,0 +1,49 @@
+package com.stu.helloserver.common;
+
+public class Result<T> {
+    private int code;
+    private String msg;
+    private T data;
+
+    public Result() {
+    }
+
+    public static <T> Result<T> success(T data) {
+        Result<T> result = new Result<>();
+        result.code = 200;
+        result.msg = "success";
+        result.data = data;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result<T> result = new Result<>();
+        result.code = 500;
+        result.msg = msg;
+        return result;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+}
---
+++ src/main/java/com/stu/helloserver/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.stu.helloserver.exception;
+
+import com.stu.helloserver.common.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result<String> handleException(Exception e) {
+        return Result.error("系统异常：" + e.getMessage());
+    }
+}
